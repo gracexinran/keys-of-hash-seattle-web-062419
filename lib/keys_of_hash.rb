ef567b6b66583921612deea844967ea6f9016a1b
Xinran Che <gracexinran@gmail.com>
2019-05-22 17:53:34 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,11 @@
 class Hash
   def keys_of(*arguments)
     # code goes here
-    
-
-  end
+    result = []
+    self.each do |k, v|
+      if arguments.include?(v)
+        result << k
+      end
+    end
+    result
 end
